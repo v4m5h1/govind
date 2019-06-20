b6c616ad9148dd23514be8592341d990206ead45
Govind D <govinddk4444@yahoo.com>
2019-06-20 09:21:41 +0530

    Merging Vamshi's Code

--- dist/action-item-slider-web-part.js
+++ dist/action-item-slider-web-part.js
@@ -23784,119 +23784,6 @@ var Def = {
 
 
 // CONCATENATED MODULE: ./lib/webparts/actionItemSlider/components/ActionItemSlider.js
-<<<<<<< HEAD
-var ActionItemSlider___extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-
-
-
-
-var ActionItemSlider_ActionItemSlider = /** @class */ (function (_super) {
-    ActionItemSlider___extends(ActionItemSlider, _super);
-    function ActionItemSlider(props, state) {
-        var _this = _super.call(this, props, state) || this;
-        _this.getPageDetails = function () {
-            var _a;
-            if (_this.state.listTitle !== "") {
-                (_a = sp$1.web.lists
-                    .getByTitle(_this.state.listTitle)
-                    .items).select.apply(_a, _this.state.selectedKeys).get()
-                    .then(function (items) {
-                    console.log(items);
-                    var images = [];
-                    items.map(function (item) {
-                        var image = {};
-                        if (item.Rollup_x0020_Image) {
-                            image = {
-                                imageUrl: item.Rollup_x0020_Image
-                                    ? item.Rollup_x0020_Image.Url
-                                    : "",
-                                title: item.Title
-                            };
-                        }
-                        if (!_this.isEmpty(image)) {
-                            images.push(image);
-                        }
-                    });
-                    _this.setState({
-                        imageMetaData: images
-                    });
-                });
-            }
-        };
-        // Set the List Title and the selected keys on Property change
-        _this.getListTitleById = function (nxtProps) {
-            sp$1.web.lists
-                .getById(nxtProps.listIds)
-                .select("Title")
-                .get()
-                .then(function (listProps) {
-                _this.setState({
-                    listTitle: listProps.Title,
-                    selectedKeys: nxtProps.selectedKeys
-                });
-                _this.getPageDetails();
-            });
-        };
-        _this.renderImages = function () {
-            // if(this.state.imageMetaData.count>0){
-            _this.state.imageMetaData.map(function (image) {
-                return (external__react_["createElement"]("div", null,
-                    external__react_["createElement"]("img", { src: image.imageUrl }),
-                    external__react_["createElement"]("p", { className: "legend" }, image.title)));
-            });
-            // return slides;
-        };
-        _this.state = {
-            imageMetaData: [],
-            listTitle: "",
-            selectedKeys: []
-        };
-        _this.getListTitleById(_this.props);
-        return _this;
-    }
-    ActionItemSlider.prototype.isEmpty = function (obj) {
-        for (var key in obj) {
-            if (obj.hasOwnProperty(key))
-                return false;
-        }
-        return true;
-    };
-    ActionItemSlider.prototype.componentWillMount = function () {
-        this.getPageDetails();
-    };
-    ActionItemSlider.prototype.componentWillReceiveProps = function (nextProps) {
-        this.getListTitleById(nextProps);
-    };
-    ActionItemSlider.prototype.render = function () {
-        console.log(this.props.selectedKeys);
-        var imageSlides = this.state.imageMetaData.map(function (image, index) {
-            return (external__react_["createElement"]("div", null,
-                external__react_["createElement"]("img", { src: image.imageUrl }),
-                external__react_["createElement"]("p", { className: "legend" }, image.title)));
-        });
-        var carousel;
-        if (this.state.imageMetaData.length > 0) {
-            carousel = (external__react_["createElement"](lib["Carousel"], { autoPlay: true, interval: 1000, infiniteLoop: true }, imageSlides));
-        }
-        else {
-            carousel = external__react_["createElement"]("span", null, "There are no Images");
-        }
-        return (external__react_["createElement"]("div", { className: ActionItemSlider_module_scss.actionItemSlider },
-            external__react_["createElement"]("div", { className: ActionItemSlider_module_scss.container }, carousel)));
-    };
-    return ActionItemSlider;
-}(external__react_["Component"]));
-/* harmony default export */ var components_ActionItemSlider = (ActionItemSlider_ActionItemSlider);
-=======
 var ActionItemSlider___extends = (this && this.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -24005,7 +23892,6 @@ var ActionItemSlider_ActionItemSlider = /** @class */ (function (_super) {
     return ActionItemSlider;
 }(external__react_["Component"]));
 /* harmony default export */ var components_ActionItemSlider = (ActionItemSlider_ActionItemSlider);
->>>>>>> f8defc49260d7e4a85e326b2eb1bcb2fa023f0b2
 
 // EXTERNAL MODULE: ./node_modules/react-responsive-carousel/lib/styles/carousel.min.css
 var carousel_min = __webpack_require__(113);
